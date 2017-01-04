27568823830fa4e3de465161ed9057df6b3b4bc0
fushiminaoto <wano_fushimi@icloud.com>
2017-01-04 12:01:00 +0900

    module test for ava

--- test/spec/test01.js
+++ test/spec/test01.js
@@ -1,10 +1,13 @@
-import {test} from 'ava';
-import {Hoge} from "../src/ts/Modules0";
+import test from 'ava';
+import {Hoge} from "../modules/Modules0"
 
 test.serial('Async/Await!', async (t) => {
     const res0 = Hoge.say() == "hello";
     t.true(res0 , "OK");
 
+    console.log("hoge");
     const res = 1 + 2 == 3;
     t.true(res, "success");
+    console.log("fuga");
 });
+
