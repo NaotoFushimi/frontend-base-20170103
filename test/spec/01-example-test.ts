fb929181aa25ea2a409af4b4a6a3721ab8a2113a
fushiminaoto <wano_fushimi@icloud.com>
2017-03-07 16:04:49 +0900

    add timeout to testscript

--- test/spec/01-example-test.ts
+++ test/spec/01-example-test.ts
@@ -5,5 +5,9 @@ import assert = require("power-assert");
 describe("section", ()=>{
     it("test case",  async ()=>{
         assert.equal(3 ,3)
+
+        await new Promise(r => setTimeout(r , 2000))
+        assert.equal(9 ,19)
+
     });
 });
